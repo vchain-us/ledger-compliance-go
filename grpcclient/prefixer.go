de9cc58c77daded6b76b1938d566e4a8c0d3fec4
Michele Meloni <cleaversdev@gmail.com>
2020-10-12 18:01:34 +0200

    feat: if pluginType client property is present it's added to the prefix

--- grpcclient/prefixer.go
+++ grpcclient/prefixer.go
@@ -3,10 +3,19 @@ package grpcclient
 import immuschema "github.com/codenotary/immudb/pkg/api/schema"
 
 func (c *LcClient) addLedgerPrefix(key []byte) []byte {
+	lenPrefixed := len(c.signerId) + 1 + len(key)
+	if c.PluginPrefix != "" {
+		lenPrefixed += len(c.PluginPrefix) + 1
+	}
+	var prefixed = make([]byte, lenPrefixed)
+	var pluginPrefixSpace = 0
+	if c.PluginPrefix != "" {
+		copy(prefixed[0:], c.PluginPrefix+".")
+		pluginPrefixSpace = len(c.PluginPrefix) + 1
+	}
 
-	var prefixed = make([]byte, len(c.signerId)+1+len(key))
-	copy(prefixed[0:], c.signerId+".")
-	copy(prefixed[len(c.signerId)+1:], key)
+	copy(prefixed[pluginPrefixSpace:], c.signerId+".")
+	copy(prefixed[len(c.signerId)+1+pluginPrefixSpace:], key)
 
 	return prefixed
 }
@@ -14,6 +23,9 @@ func (c *LcClient) addLedgerPrefix(key []byte) []byte {
 func (c *LcClient) stripLedgerPrefix(key []byte) []byte {
 	kLen := len(key)
 	sigIdLen := len(c.signerId) + 1 // remove also .
+	if c.PluginPrefix != "" {
+		sigIdLen += len(c.PluginPrefix) + 1
+	}
 	hashLen := kLen - (sigIdLen)
 	var prefixed = make([]byte, hashLen)
 	copy(prefixed[:], key[sigIdLen:])
