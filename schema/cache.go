d0e3ba22fbe8a50e9d23917174f20214637cbc32
Michele Meloni <cleaversdev@gmail.com>
2022-03-24 15:22:28 +0100

    feat:VCNSearchRequest implements cacheable interface, fix VCNArtifactsGetRequest key generation

--- schema/cache.go
+++ schema/cache.go
@@ -8,11 +8,30 @@ import (
 )
 
 func (r *VCNArtifactsGetRequest) GetKey(prefix ...string) string {
-	pieces := make([]string, 0, len(r.Hashes)+len(prefix))
-	pieces = append(pieces, prefix...)
+	pieces := make([]string, len(r.Hashes))
 	copy(pieces, r.Hashes)
+	pieces = append(pieces, prefix...)
 	sort.Strings(pieces)
 	pieces = append(pieces, fmt.Sprintf("%d", r.GetProveSinceTx()))
+	fullKey := strings.Join(pieces, "")
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(fullKey)))
+}
+
+func (r *VCNSearchRequest) GetKey(prefix ...string) string {
+	pieces := make([]string, 0)
+	pieces = append(pieces, prefix...)
+	pieces = append(pieces, r.Hash)
+	pieces = append(pieces, r.SignerID)
+	pieces = append(pieces, r.AType.String())
+	pieces = append(pieces, r.Type.String())
+	pieces = append(pieces, fmt.Sprintf("%d", r.Limit))
+	pieces = append(pieces, fmt.Sprintf("%t", r.Desc))
+	pieces = append(pieces, fmt.Sprintf("%d", r.StartTS))
+	pieces = append(pieces, fmt.Sprintf("%d", r.EndTS))
+	pieces = append(pieces, r.AttachName)
+	pieces = append(pieces, r.ArtifactName)
+	pieces = append(pieces, r.UID)
+	sort.Strings(pieces)
 	fullKey := strings.Join(pieces, "")
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(fullKey)))
 }
