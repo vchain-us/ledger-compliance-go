558082d3907be2df00361c2c953b983d0860500e
Artur Melanchyk <artur@codenotary.com>
2022-06-09 09:50:59 +0200

    set total retry time ~10sec

--- grpcclient/lc_client.go
+++ grpcclient/lc_client.go
@@ -169,7 +169,7 @@ func NewLcClient(setters ...LcClientOption) *LcClient {
 
 	retryOpts := []grpc_retry.CallOption{
 		grpc_retry.WithMax(3),
-		grpc_retry.WithBackoff(grpc_retry.BackoffExponential(500 * time.Millisecond)),
+		grpc_retry.WithBackoff(grpc_retry.BackoffExponential(1500 * time.Millisecond)),
 		grpc_retry.WithCodes(codes.Aborted, codes.Unavailable, codes.Unknown),
 	}
 
